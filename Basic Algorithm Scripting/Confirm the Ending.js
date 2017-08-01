378e17b6c15d658f1ee5fb0e910f09ef24e7a934
Matt Peters <matthew.george.peters@gmail.com>
2017-08-01 19:21:13 -0400

    js linted conf ending

--- Basic Algorithm Scripting/Confirm the Ending.js
+++ Basic Algorithm Scripting/Confirm the Ending.js
@@ -4,15 +4,17 @@
 This challenge can be solved with the .endsWith() method, which was 
 introduced in ES2015. But for the purpose of this challenge, we would 
 like you to use one of the JavaScript substring methods instead. */
-
+"use strict";
 function confirmEnding(str, target) {
-  var strLength = str.length;
-  var targetLength = target.length;
-  
-  var strEndLetter = str.substr((strLength - targetLength), strLength);
-  var targetEndLetter = target.substr((targetLength-strLength), targetLength);
-  
-  return targetEndLetter === strEndLetter;
-}
 
+    var strLength = str.length;
+
+    var targetLength = target.length;
+
+    var strEndLetter = str.substr((strLength - targetLength), strLength);
+
+    var targetEndLetter = target.substr((targetLength - strLength), targetLength);
+
+    return targetEndLetter === strEndLetter;
+}
 confirmEnding("He has to give me a new name", "name");
