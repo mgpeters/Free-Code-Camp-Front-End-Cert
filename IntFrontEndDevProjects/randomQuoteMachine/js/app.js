a0d0ce8849766cc474078077af6227d7055ffc4b
Matt Peters <matthew.george.peters@gmail.com>
2017-09-24 18:46:43 -0400

    fixed tweet link and formatting

--- IntFrontEndDevProjects/randomQuoteMachine/js/app.js
+++ IntFrontEndDevProjects/randomQuoteMachine/js/app.js
@@ -5,9 +5,23 @@ $(document).ready(function(){
 	  $.ajax( {
 		url: 'http://www.quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
       	success: function(data) {
-        	var post = data.shift(); // The data is an array of posts. Grab the first one.
+        	var post = data.shift(), // The data is an array of posts. Grab the first one.
+        		title = post.title,
+        		str = post.content,
+        		quote = str.replace("<p>",'').replace("</p>", '')
+        			.replace("&#8217;", "'").replace("&#8230;", "...").replace("&#8220;", '"')
+        				.replace("&#8221", '"').replace("<br />", "");
+        		console.log(quote);
         	$('#quote-title').text(post.title);
         	$('#quote-content').html(post.content);
+        	$('.twitter-share-button').on('click', function(){
+        		if(quote.length < 140){
+        			$(this).attr('href', 'https://twitter.com/intent/tweet?text=' + '"' + quote + '"' + " - " + title);
+        		}
+        		else{
+        			alert("This quote has too many characters")
+        		}
+        	});
         	// If the Source is available, use it. Otherwise hide it.
         	if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
           		$('#quote-source').html('Source: ' + post.custom_meta.Source);
