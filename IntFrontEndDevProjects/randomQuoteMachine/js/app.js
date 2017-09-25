dd02a22ee0fd7410bec6827a81e37cdd24cb4d84
Matt Peters <matthew.george.peters@gmail.com>
2017-09-25 18:53:45 -0400

    tweeked the id items padding

--- IntFrontEndDevProjects/randomQuoteMachine/js/app.js
+++ IntFrontEndDevProjects/randomQuoteMachine/js/app.js
@@ -13,7 +13,6 @@ $(document).ready(function(){
         				.replace("&#8221", '"').replace("<br />", "");
         		console.log(quote);
         	$('.twitter-share-button').on('click', function(){
-        		str = 
         		if(quote.length >= 141){
         			return alert("This quote has too many characters");
         		}
