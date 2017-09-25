3ba150c09aeff915da70a0a5d53500182a78d71b
Matt Peters <matthew.george.peters@gmail.com>
2017-09-25 17:53:18 -0400

    attempted to debug tweet button.

--- IntFrontEndDevProjects/randomQuoteMachine/js/app.js
+++ IntFrontEndDevProjects/randomQuoteMachine/js/app.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
         			.replace("&#8217;", "'").replace("&#8230;", "...").replace("&#8220;", '"')
         				.replace("&#8221", '"').replace("<br />", "");
         		console.log(quote);
+        	$('.twitter-share-button').on('click', function(){
+        		if(quote.length >= 141){
+        			return alert("This quote has too many characters");
+        		}
+        	    if(quote.length <= 140){
+        	    	$(this).attr('href', 'https://twitter.com/intent/tweet?text=' + '"' + quote + '"' + " - " + title);
+        			}
+        		});
         	$('#quote-title').text(post.title);
         	$('#quote-content').html(post.content);
-        	$('.twitter-share-button').on('click', function(){
-        		if(quote.length < 140){
-        			$(this).attr('href', 'https://twitter.com/intent/tweet?text=' + '"' + quote + '"' + " - " + title);
-        		}
-        		else{
-        			return alert("This quote has too many characters")
-        		}
-        	});
         	// If the Source is available, use it. Otherwise hide it.
         	if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
           		$('#quote-source').html('Source: ' + post.custom_meta.Source);
