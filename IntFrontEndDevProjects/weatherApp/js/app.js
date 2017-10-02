21e0d3aaa6e5bd08e88e5113c554d3eba8947bc1
Matt Peters <matthew.george.peters@gmail.com>
2017-10-02 18:28:22 -0400

    setting up ajax var's

--- IntFrontEndDevProjects/weatherApp/js/app.js
+++ IntFrontEndDevProjects/weatherApp/js/app.js
@@ -1,12 +1,17 @@
 $(document).foundation()
 $(document).ready(function(){
-	  e.preventDefault();
 	  $.ajax( {
 		url: 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139',
       	success: function(data) {
-      		var post = data.shift()
-	        	$('#quote-title').html(title);
-	        	$('#quote-content').html(quote);
+      		console.log(data);
+      		var weatherTemp = data.main.temp,
+      			weatherMain = data.weather[0].main,
+      			weatherDesc = data.weather[0].description,
+      			weatherIcon = data.weather[0].icon;
+      			console.log(weatherTemp);
+
+	        	/*$('#quote-title').html(title);
+	        	$('#quote-content').html(quote);*/
 	      		},
       		cache: false
     	});
