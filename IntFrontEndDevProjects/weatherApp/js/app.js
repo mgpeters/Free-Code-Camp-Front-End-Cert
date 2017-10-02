6ce10ca72e4bf855a04cecbf84acf2de1547b0fb
Matt Peters <matthew.george.peters@gmail.com>
2017-10-02 18:06:22 -0400

    added .ajax frame

--- IntFrontEndDevProjects/weatherApp/js/app.js
+++ IntFrontEndDevProjects/weatherApp/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+$(document).ready(function(){
+	  e.preventDefault();
+	  $.ajax( {
+		url: 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139',
+      	success: function(data) {
+      		var post = data.shift()
+	        	$('#quote-title').html(title);
+	        	$('#quote-content').html(quote);
+	      		},
+      		cache: false
+    	});
+});
