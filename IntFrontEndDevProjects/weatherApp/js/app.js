7abaff625f8751d8aa15d6d6d133bbe8b7322b97
Matt Peters <matthew.george.peters@gmail.com>
2017-10-02 18:47:07 -0400

    linked jquery and .html

--- IntFrontEndDevProjects/weatherApp/js/app.js
+++ IntFrontEndDevProjects/weatherApp/js/app.js
@@ -3,21 +3,20 @@ $(document).ready(function(){
 	  $.ajax( {
 		url: 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139',
       	success: function(data) {
+      		// variables to dump JSON API data in
       		var weatherTemp = data.main.temp,
       			weatherMain = data.weather[0].main,
       			weatherDesc = data.weather[0].description,
       			weatherIcon = data.weather[0].icon;
       			weatherCountry = data.sys.country,
       			weatherCity = data.name;
-      			console.log(weatherTemp);
-      			console.log(weatherMain);
-      			console.log(weatherDesc);
-      			console.log(weatherIcon);
-      			console.log(weatherCountry);
-      			console.log(weatherCity);
-
-	        	$('#temp').html(weatherTemp);
-	        	/*$('#quote-content').html(quote);*/
+      		// calls to manipulate the DOM
+	        	$('#weather-temp').html(weatherTemp);
+	        	$('#weather-main').html(weatherMain);
+	        	$('#weather-desc').html(weatherDesc);
+	        	$('#weather-icon').html('<img src="' + weatherIcon + '" ></img>');
+	        	$('#weather-country').html(weatherCountry);
+	        	$('#weather-city').html(weatherCity);
 	      		},
       		cache: false
     	});
