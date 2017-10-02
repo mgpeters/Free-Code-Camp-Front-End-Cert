e610ff1874b05cf822f9e7c294e9fb4117642334
Matt Peters <matthew.george.peters@gmail.com>
2017-10-02 18:42:06 -0400

    updated div id's

--- IntFrontEndDevProjects/weatherApp/js/app.js
+++ IntFrontEndDevProjects/weatherApp/js/app.js
@@ -3,15 +3,21 @@ $(document).ready(function(){
 	  $.ajax( {
 		url: 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139',
       	success: function(data) {
-      		console.log(data);
       		var weatherTemp = data.main.temp,
       			weatherMain = data.weather[0].main,
       			weatherDesc = data.weather[0].description,
       			weatherIcon = data.weather[0].icon;
+      			weatherCountry = data.sys.country,
+      			weatherCity = data.name;
       			console.log(weatherTemp);
+      			console.log(weatherMain);
+      			console.log(weatherDesc);
+      			console.log(weatherIcon);
+      			console.log(weatherCountry);
+      			console.log(weatherCity);
 
-	        	/*$('#quote-title').html(title);
-	        	$('#quote-content').html(quote);*/
+	        	$('#temp').html(weatherTemp);
+	        	/*$('#quote-content').html(quote);*/
 	      		},
       		cache: false
     	});
