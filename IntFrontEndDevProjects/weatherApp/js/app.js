f0a1a8c14bec42d8fd7b44d09e0a6a2982b80924
Matt Peters <matthew.george.peters@gmail.com>
2017-10-11 17:42:50 -0400

    cleaned up weather desc

--- IntFrontEndDevProjects/weatherApp/js/app.js
+++ IntFrontEndDevProjects/weatherApp/js/app.js
@@ -16,7 +16,6 @@ $(document).ready(function(){
 	      		// variables to dump JSON API data in
 	      		var weatherTemp = data.main.temp,
 	      			weatherMain = data.weather[0].main,
-	      			weatherDesc = data.weather[0].description,
 	      			weatherIcon = data.weather[0].icon,
 	      			weatherCountry = data.sys.country,
 	      			weatherCity = data.name;
@@ -35,7 +34,7 @@ $(document).ready(function(){
 
 	      		// calls to manipulate the DOM
 		        	$('#weather-temp').html(weatherTemp);
-		        	$('#weather-main').html(weatherMain + " - " + weatherDesc);
+		        	$('#weather-main').html(weatherMain);
 		        	$('#weather-icon').html('<img src="' + weatherIcon + '"></img>');
 		        	$('#weather-country').html(weatherCity + " - " + weatherCountry);
 		      		},
