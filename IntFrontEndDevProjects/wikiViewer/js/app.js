c67283b59b9729a751f459451fdd5a25e8471ee1
Matt Peters <matthew.george.peters@gmail.com>
2017-10-18 19:58:49 -0400

    added ajax url // wrapped in click function

--- IntFrontEndDevProjects/wikiViewer/js/app.js
+++ IntFrontEndDevProjects/wikiViewer/js/app.js
@@ -1,7 +1,8 @@
 $(document).foundation()
 $(document).ready(function(){
+	$().on('click', function(){
 	  $.ajax( {
-		url: '',
+		url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
       	success: function(data) {
         	
         			
@@ -9,5 +10,6 @@ $(document).ready(function(){
       		},
       	cache: false
     	});
+	})  
 
 })
