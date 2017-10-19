5faacf8e2299d22032ea0a948d9e019cd864240b
Matt Peters <matthew.george.peters@gmail.com>
2017-10-19 17:11:26 -0400

    ajax buildup

--- IntFrontEndDevProjects/wikiViewer/js/app.js
+++ IntFrontEndDevProjects/wikiViewer/js/app.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
 	  $.ajax( {
 		url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
       	success: function(data) {
-        	
+        	var title,
+        		snippet,
+        		pageUrl;
+
         			
       
       		},
