7e4de767a20be7ed5c5e4b27a6471e8ae61b2309
Matt Peters <matthew.george.peters@gmail.com>
2017-10-19 17:13:55 -0400

    for in ajax

--- IntFrontEndDevProjects/wikiViewer/js/app.js
+++ IntFrontEndDevProjects/wikiViewer/js/app.js
@@ -4,12 +4,13 @@ $(document).ready(function(){
 	  $.ajax( {
 		url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
       	success: function(data) {
-        	var title,
-        		snippet,
-        		pageUrl;
-
-        			
-      
+        	var title = [],
+        		snippet = [],
+        		pageUrl = [],
+        		x = 0;
+        		for (x; x < data.query.search; x + 1){
+        			title[x] = 
+        		}
       		},
       	cache: false
     	});
