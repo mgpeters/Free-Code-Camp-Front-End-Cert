40575812d46a7278c8bf5847ffadbb5e1abd868e
Matt Peters <matthew.george.peters@gmail.com>
2017-10-19 17:21:26 -0400

    testing ajax call

--- IntFrontEndDevProjects/wikiViewer/js/app.js
+++ IntFrontEndDevProjects/wikiViewer/js/app.js
@@ -1,6 +1,6 @@
 $(document).foundation()
 $(document).ready(function(){
-	$().on('click', function(){
+	//$().on('click', function(){
 	  $.ajax( {
 		url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
       	success: function(data) {
@@ -9,11 +9,13 @@ $(document).ready(function(){
         		pageUrl = [],
         		x = 0;
         		for (x; x < data.query.search; x + 1){
-        			title[x] = 
+        			title[x] = data.query.search.title;
+        			snippet[x] = data.query.search.snippet;
+        			pageUrl[x] = 'https://en.wikipedia.org/wiki/' + title;
         		}
       		},
       	cache: false
     	});
-	})  
+	//})  
 
 })
