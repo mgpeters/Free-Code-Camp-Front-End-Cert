d608460d5297f5fe7eda354ec12400ef441a4a8e
Matt Peters <matthew.george.peters@gmail.com>
2017-10-20 17:34:39 -0400

    ajax call kinda working

--- IntFrontEndDevProjects/wikiViewer/js/app.js
+++ IntFrontEndDevProjects/wikiViewer/js/app.js
@@ -2,18 +2,19 @@ $(document).foundation()
 $(document).ready(function(){
 	//$().on('click', function(){
 	  $.ajax( {
-		url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=futon&prop=info&inprop=url&utf8=&format=json&origin=*',
-		type: 'json',
+	  	dataType: 'json',
+		url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=futon&prop=info&origin=*&inprop=url&utf8=&format=json',
       	success: function(data) {
+      		console.log(data);
         	var title = [],
         		snippet = [],
         		pageUrl = [],
         		x = 0;
-        		for (x; x < data.query.search.length; x + 1){
+        		/*for (x; x < data.query.search.length; x + 1){
         			title[x] = data.query.search.title;
         			snippet[x] = data.query.search.snippet;
         			pageUrl[x] = 'https://en.wikipedia.org/wiki/' + title;
-        		}
+        		}*/
         		console.log(title);
         		console.log(snippet);
         		console.log(pageUrl);
