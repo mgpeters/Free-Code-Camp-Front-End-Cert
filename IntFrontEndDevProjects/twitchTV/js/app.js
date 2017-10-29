15e1d615ba8b8248dbb0c30ed60615f4350f9f22
Matt Peters <matthew.george.peters@gmail.com>
2017-10-29 12:05:59 -0400

    made a gerData function

--- IntFrontEndDevProjects/twitchTV/js/app.js
+++ IntFrontEndDevProjects/twitchTV/js/app.js
@@ -1,12 +1,17 @@
 $(document).foundation()
+
+function getData(user){
+		return $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?', function(data){
+	}
+}
+
 $(document).ready(function(){
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
 		jsonAPI = [],
 		y = 0;
 
-			for(y; y < users.length; y += 1){
-			jsonAPI.push($.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[y] + '?callback=?', function(data){
-				console.log("success!! - " + data);}))
-			}
+		for(y;y > users.length; y += 1){
+			getData(users[y])
+		}
 		console.log(jsonAPI);
 })
