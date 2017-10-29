1bb230ba0cda4bf40fe81b232bd437a0f590066d
Matt Peters <matthew.george.peters@gmail.com>
2017-10-29 10:53:35 -0400

    started javascript stuff

--- IntFrontEndDevProjects/twitchTV/js/app.js
+++ IntFrontEndDevProjects/twitchTV/js/app.js
@@ -1,6 +1,9 @@
 $(document).foundation()
 $(document).ready(function(){
+	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
+		jsonAIP = [];
+
 	$.getJSON('https://wind-bow.gomix.me/twitch-api/users/freecodecamp?callback=?', function(data){
-		console.log(data);
-	})
+		users.push(data);
+	console.log(users);
 })
