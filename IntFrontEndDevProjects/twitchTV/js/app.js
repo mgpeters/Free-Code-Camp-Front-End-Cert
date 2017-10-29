2e9ab53ec1df756338ded1ae1def730076d7b669
Matt Peters <matthew.george.peters@gmail.com>
2017-10-29 11:55:01 -0400

    working js but its broke

--- IntFrontEndDevProjects/twitchTV/js/app.js
+++ IntFrontEndDevProjects/twitchTV/js/app.js
@@ -1,9 +1,12 @@
 $(document).foundation()
 $(document).ready(function(){
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
-		jsonAIP = [];
+		jsonAPI = [],
+		y = 0;
 
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/users/freecodecamp?callback=?', function(data){
-		users.push(data);
-	console.log(users);
+			for(y; y < users.length; y += 1){
+			jsonAPI.push($.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[y] + '?callback=?', function(data){
+				console.log("success!! - " + data);}))
+			}
+		console.log(jsonAPI);
 })
