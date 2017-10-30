8ff9da4e70f849135243e5bba538999d155ecf2f
Matt Peters <matthew.george.peters@gmail.com>
2017-10-30 17:50:01 -0400

    js is a mess

--- IntFrontEndDevProjects/twitchTV/js/app.js
+++ IntFrontEndDevProjects/twitchTV/js/app.js
@@ -1,12 +1,23 @@
 $(document).foundation()
 
 function getData(user){
-		return $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?', function(data){return data})
-	}
+	 $.ajax({
+    		url: 'https://wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?',
+    		async: true,
+    		dataType: "json", 
+    		success: function(data){
+    			console.log(data)
+        		jsonAPI.push(data);
+     		}
+	});
+};
 
 $(document).ready(function(){
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
-		jsonAPI = [],
-		y = 0;
+		y = 0,
+		jsonAPI = new Array();
+      console.log(typeof jsonAPI);
+		getData(users[0]);
+		console.log(jsonAPI);
 
 })
