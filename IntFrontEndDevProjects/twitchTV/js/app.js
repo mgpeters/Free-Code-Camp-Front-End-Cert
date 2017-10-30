2d62c3c07ad52d0381cd112483609b1480d97bd4
Matt Peters <matthew.george.peters@gmail.com>
2017-10-30 18:14:36 -0400

    got them into the array

--- IntFrontEndDevProjects/twitchTV/js/app.js
+++ IntFrontEndDevProjects/twitchTV/js/app.js
@@ -3,7 +3,15 @@ $(document).foundation()
 
 $(document).ready(function(){
 	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
-		jsonAPI = new Array();
+		x = 0;
+		jsonAPI = [];
 
+		for (x; x < users.length; x += 1){
+			$.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[x] + '?callback=?', function(data){
+				jsonAPI.push(data);
+			})
+		}
+		console.log(jsonAPI);
 
-})
+ })
+
