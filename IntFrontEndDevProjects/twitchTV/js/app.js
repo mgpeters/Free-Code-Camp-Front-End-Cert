ed54f9437fc983327c8d8b1bbe55c6fb59eebfa4
Matt Peters <matthew.george.peters@gmail.com>
2017-11-01 16:38:58 -0400

    working js

--- IntFrontEndDevProjects/twitchTV/js/app.js
+++ IntFrontEndDevProjects/twitchTV/js/app.js
@@ -2,8 +2,9 @@ $(document).foundation()
 
 
 $(document).ready(function(){
-	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
-		x = 0;
+	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "FreeCodeCamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
+		x = 0,
+		y = 0,
 		userData = [],
 		userObject = {};
 
@@ -11,26 +12,25 @@ $(document).ready(function(){
 			$.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?', function(data){
 				var id = data.display_name
 				userObject[id] = data;
-				userData.push(data);
 			})
-			/*$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?', function(data){
-				userData.push(data);
-			})*/
 		}
 
-		/*function getUserStatus(user){
+		function getUserStatus(user){
 			$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?', function(data){
-				userStatus.push(data);
+				userObject[user].stream = data.stream;
 			})
 		}
 
 		function newUser(nUser){
 			users.push(nUser);
 			getUserData(nUser);
-		}*/
+		}
 
 		for (x; x < users.length; x += 1){
 				getUserData(users[x]);
+			}
+		for (y; y < users.length; y += 1){
+				getUserStatus(users[y]);
 			}
 
 		console.log(userData);
