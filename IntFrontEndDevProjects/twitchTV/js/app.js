3f2d97810e83b1fba470dcf709bed25aa26e8633
Matt Peters <matthew.george.peters@gmail.com>
2017-11-06 19:21:46 -0500

    added some features

--- IntFrontEndDevProjects/twitchTV/js/app.js
+++ IntFrontEndDevProjects/twitchTV/js/app.js
@@ -10,8 +10,10 @@ $(document).ready(function(){
 		function getUserData(user){ //gets the bulk of the api data I want to play around with
 			$.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?', function(data){
 				var id = data.display_name;
+					if (data == undefined){
+						alert("User Not Found");
+					}
 					userObject[id] = JSON.parse(JSON.stringify(data)),
-
 					getUserStatus(user);
 			})
 		}
@@ -19,14 +21,32 @@ $(document).ready(function(){
 		function getUserStatus(user){ //needed to band-aid the async problem and to add the steam status
 			$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?', function(data){
 				userObject[user].status = data.stream;
-				status = data.status;
 			})
 		}
 
 		function newUser(nUser){ //made but not implemented yet.
 			users.push(nUser);
 			getUserData(nUser);
-		}
+
+				setTimeout(function(){
+					let name = userObject[nUser].name,
+							bio = userObject[nUser].bio,
+							logo = userObject[nUser].logo,
+							links = "http://go.twitch.tv/" + name,
+							online = '';
+
+							if (userObject[users[y]].status == null){
+								online = "Offline";
+							}
+							else {
+								online = "Online";
+							}
+
+						$('#user-div')
+		        		.append('<div class="user-result-cell"><a href="' + links + '">' + name + '</a> ' + online + '<br/><h6>' 
+		        					+ bio + '</h6></div>')
+				}, 1000)
+			}
 
 		function objectTest(data){
 			console.log(userObject); //checks to see if my object is doing what I want
@@ -53,8 +73,9 @@ $(document).ready(function(){
 						online = "Online";
 					}
 
-					$('#user-div')
-        		.append('<div class="user-result-cell"><a href="' + links + '">' + name + '</a> ' + online + '<br/><h6>' + bio + '</h6></div>')
+				$('#user-div')
+        		.append('<div class="user-result-cell"><a href="' + links + '">' + name + '</a> ' + online + '<br/><h6>' 
+        					+ bio + '</h6></div>')
 				}
 
 		}, 2000)
