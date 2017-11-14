6bf9672b951ad85e741f1e8de56a1e85ab35d98c
Matt Peters <matthew.george.peters@gmail.com>
2017-11-14 17:25:01 -0500

    cleaned up console logs

--- Intermediate Algorithm Scripting/RomanNumeralConverter.js
+++ Intermediate Algorithm Scripting/RomanNumeralConverter.js
@@ -12,7 +12,6 @@ function convertToRoman(num) {
         if(workNum >= 1000){
             roman += "M";
             workNum = workNum - 1000;
-            console.log(input);
             recursiveConvert(workNum);
         }
         else if(workNum >= 900) {
@@ -33,6 +32,46 @@ function convertToRoman(num) {
         else if(workNum >= 100) {
         	roman += "C";
         	workNum = workNum - 100;
+        	recursiveConvert(workNum);
+        }
+        else if(workNum >= 90){
+            roman += "XC";
+            workNum = workNum - 90;
+            recursiveConvert(workNum);
+        }
+        else if(workNum >= 50) {
+        	roman += "L";
+        	workNum = workNum - 50;
+        	recursiveConvert(workNum);
+        }
+        else if(workNum >= 40) {
+        	roman += "XL";
+        	workNum = workNum - 40;
+        	recursiveConvert(workNum);
+        }
+        else if(workNum >= 10) {
+        	roman += "X";
+        	workNum = workNum - 10;
+        	recursiveConvert(workNum);
+        }
+        else if(workNum >= 9) {
+        	roman += "IX";
+        	workNum = workNum - 9;
+        	recursiveConvert(workNum);
+        } 
+        else if(workNum >= 5) {
+        	roman += "V";
+        	workNum = workNum - 5;
+        	recursiveConvert(workNum);
+        }
+        else if(workNum >= 4) {
+        	roman += "IV";
+        	workNum = workNum - 4;
+        	recursiveConvert(workNum);
+        }
+        else if(workNum >= 1) {
+        	roman += "I";
+        	workNum = workNum - 1;
         	recursiveConvert(workNum);
         }
 	}
