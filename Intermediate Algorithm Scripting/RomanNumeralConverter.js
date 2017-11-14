94059d300aba87406078e25848178f568693f8a8
Matt Peters <matthew.george.peters@gmail.com>
2017-11-14 17:06:04 -0500

    making the recursive function within the funtion

--- Intermediate Algorithm Scripting/RomanNumeralConverter.js
+++ Intermediate Algorithm Scripting/RomanNumeralConverter.js
@@ -1,3 +1,22 @@
 //Convert the given number into a roman numeral.
 //All roman numerals answers should be provided in upper-case.
 
+
+ 
+function convertToRoman(num) {
+	var roman = "";
+
+	function recursiveConvert (input){
+    	let workNum = num;
+
+        if(workNum > 1000){
+
+            arr.push("M");
+            workNum = workNum - 1000;
+            convertToRoman(workNum);
+        }
+	} 
+return roman;
+}
+ 
+convertToRoman(5000);
