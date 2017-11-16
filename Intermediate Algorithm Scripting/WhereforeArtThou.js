2c30bb914d8693d92ccb6654262e10aafec63687
Matt Peters <matthew.george.peters@gmail.com>
2017-11-16 18:35:32 -0500

    now part two...

--- Intermediate Algorithm Scripting/WhereforeArtThou.js
+++ Intermediate Algorithm Scripting/WhereforeArtThou.js
@@ -48,3 +48,5 @@ function whatIsInAName(collection, source) {
 }
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet"});
+
+
