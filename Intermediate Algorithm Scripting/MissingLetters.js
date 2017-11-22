41eb55221ef60efd077a889f338b5969bc6c6ba6
Matt Peters <matthew.george.peters@gmail.com>
2017-11-22 18:00:28 -0500

    almost there

--- Intermediate Algorithm Scripting/MissingLetters.js
+++ Intermediate Algorithm Scripting/MissingLetters.js
@@ -7,7 +7,23 @@ If all letters are present in the range, return undefined.
 */
 
 function fearNotLetter(str) {
-  return str;
+	var numberArray = [],
+		round1 = 0,
+		round2 = 0;
+
+		console.log(str.charCodeAt(round1));
+
+	for(round1; round1 < str.length; round1+= 1){
+		numberArray.push(str.charCodeAt(round1));
+	}
+	for(round2; round2 < numberArray.length; round2 += 1){
+		if(numberArray[round2] + 1 === numberArray[round2 + 1]){
+			return String.fromCharCode(numberArray[round2] + 1);
+		}
+	}
+	console.log(numberArray);
+
+  return undefined;
 }
 
 fearNotLetter("abce");
