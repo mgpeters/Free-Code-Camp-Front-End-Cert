8302950fb1b44cc19a3838381bbfb0a48f124436
Matt Peters <matthew.george.peters@gmail.com>
2017-11-23 09:32:34 -0500

    on the right track

--- Intermediate Algorithm Scripting/MissingLetters.js
+++ Intermediate Algorithm Scripting/MissingLetters.js
@@ -8,22 +8,33 @@ If all letters are present in the range, return undefined.
 
 function fearNotLetter(str) {
 	var numberArray = [],
-		round1 = 0,
+		round = 0,
 		round2 = 0;
 
-		console.log(str.charCodeAt(round1));
+		console.log(str.charCodeAt(3));
 
-	for(round1; round1 < str.length; round1+= 1){
+	/*for(round1; round1 < str.length; round1+= 1){
 		numberArray.push(str.charCodeAt(round1));
 	}
 	for(round2; round2 < numberArray.length; round2 += 1){
-		if(numberArray[round2] - numberArray[round] !== 1){
+		if(str.charCodeAt(round2) !== numberArray[round2]){
 			return String.fromCharCode(numberArray[round2] + 1);
 		}
 	}
 	console.log(numberArray);
+	*/
 
+	for(round; round < str.length; round += 1){
+		if(isNaN(str.charCodeAt(round))){
+			console.log("NaN/Undefined Works");
+			return undefined;
+		}
+		else if(str.charCodeAt(round + 1) - str.charCodeAt(round) !== 1){
+			console.log("elseif works on round: " + round);
+			return String.fromCharCode(str.charCodeAt(round) + 1);
+		}
+	}
   return undefined;
 }
 
-fearNotLetter("bcd")
+fearNotLetter("abce");
