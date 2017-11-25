e338fdab20079e15b677d367f6bd4709f449850b
Matt Peters <matthew.george.peters@gmail.com>
2017-11-25 15:28:28 -0500

    cleaned up boo hoo

--- Intermediate Algorithm Scripting/BooWho.js
+++ Intermediate Algorithm Scripting/BooWho.js
@@ -8,12 +8,7 @@ Boolean primitives are true and false.
 
 function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
-	if(typeof bool === "boolean"){
-		return true;
-	}
-	else{
-		return false;
-	}
+	return typeof bool === "boolean";
 }
 
 booWho(false);
