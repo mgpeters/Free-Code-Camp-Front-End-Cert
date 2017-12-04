fd9c130ffe28f9acc3932edc87e50d86f4badcdb
Matt Peters <matthew.george.peters@gmail.com>
2017-12-04 14:50:43 -0500

    all but one

--- Intermediate Algorithm Scripting/ConvertHtmlEntities.js
+++ Intermediate Algorithm Scripting/ConvertHtmlEntities.js
@@ -6,7 +6,7 @@ Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a strin
 
 function convertHTML(str) {
   // &colon;&rpar;\
-	var	regExpression = /&|<|>|"|'*/g,
+	var	regExpression = /&|<|>|"|'/g,
 		found = regExpression.exec(str);
 		regexKey = {};
 		
@@ -17,9 +17,13 @@ function convertHTML(str) {
 		regexKey[/'/] = '&apos;';
 
 		console.log(found);
-
+  
+        if (found === null){
+          return str;
+        }
   
   return str.replace(regExpression, regexKey['/' + found[0] + '/']);
 }
 
-convertHTML("Dolce & Gabbana");
+
+convertHTML("abhhc");
