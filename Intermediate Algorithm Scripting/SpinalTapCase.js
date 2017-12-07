ca0000cebc2743f454efdd640d085f1c24e9858f
Matt Peters <matthew.george.peters@gmail.com>
2017-12-07 15:25:03 -0500

    started spinal tap case

---
+++ Intermediate Algorithm Scripting/SpinalTapCase.js
@@ -0,0 +1,13 @@
+/*
+Spinal Tap Case
+
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+*/
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  return str;
+}
+
+spinalCase('This Is Spinal Tap');
