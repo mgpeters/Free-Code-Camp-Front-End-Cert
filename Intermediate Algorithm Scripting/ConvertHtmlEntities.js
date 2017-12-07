90adc1033744e78c9f8211989f2b07df73a71c8f
Matt Peters <matthew.george.peters@gmail.com>
2017-12-07 15:22:34 -0500

    completed challange

--- Intermediate Algorithm Scripting/ConvertHtmlEntities.js
+++ Intermediate Algorithm Scripting/ConvertHtmlEntities.js
@@ -4,44 +4,23 @@ Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a strin
 */
 
 function convertHTML(str) {
-  // &colon;&rpar;\
-	var	regExpression = /&|<|>|"|'/g,
-		convert = "",
-		regexKey = {};
-		
-		regexKey[/&/] = '&amp;';
-		regexKey[/</] = '&lt;';
-		regexKey[/>/] = '&gt;';
-		regexKey[/"/] = '&quot;';
-		regexKey[/'/] = '&apos;';
+  
+	function switchHTML(input){
+  		switch(input){
+    		case "&":
+      			return "&amp;";
+    		case "<":
+      			return "&lt;"; 
+    		case ">":
+      			return "&gt;";
+    		case '"':
+      			return '&quot;';
+    		case "'":
+      			return "&apos;";  
+    }
+ }
 
-		//console.log(typeof regExpression.exec(str) === object);
-
-		function filterREGEX(input){
-			let found = regExpression.exec(input);
-
-			if(found === null){
-				console.log("nothing to change: " + found);
-				convert = input;
-				return;
-			}
-			else if(regExpression.test(input)){
-				convert = input.replace(regExpression, regexKey['/' + found[0] + '/'])
-				console.log("Change Detected: " + regExpression);
-				filterREGEX(convert);
-			}
-			else{
-				console.log("Broke Recursion");
-				return input;
-			}
-
-		}
-//.replace(regExpression, regexKey['/' + found[0] + '/']);
-	filterREGEX(str);
-
-	return convert;
+	return str.replace(/&|<|>|"|'/g, switchHTML);
 }
 
-
-convertHTML("abhhc");
-
+convertHTML("Dolce & Gabbana");
