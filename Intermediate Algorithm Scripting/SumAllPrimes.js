edc840e64e18a41c16cab2710f821b4699788a11
Matt Peters <matthew.george.peters@gmail.com>
2017-12-28 17:53:30 -0500

    working problem

--- Intermediate Algorithm Scripting/SumAllPrimes.js
+++ Intermediate Algorithm Scripting/SumAllPrimes.js
@@ -13,6 +13,7 @@ The provided number may not be a prime.
 
 function sumPrimes(num) {
 	var bucket = [], //bucket list of numbers up to num
+		sieveBucket = [],
 		bucketRound = 2,
 		round1 = 0;
 
@@ -22,21 +23,28 @@ function sumPrimes(num) {
 
 	console.log("Your bucket contains: " + bucket);
 
+	sieveBucket = bucket;
+
 	for(round1; round1 < bucket.length; round1 += 1){ //Sieve of Eratosthenes
 		for(let round2 = 0; round2 < bucket.length; round2 += 1){
-		let multiple = bucket[round1] * bucket[round2];
+			console.log("round1 is now: " + round1 + " which contains: " + bucket[round1] + " while round2 is now: " + round2 + " which contains: " + bucket[round2]);
+			let multiple = bucket[round1] * bucket[round2];
+			console.log("var multiple is: " + multiple);
+
+			console.log("Here's the index of bucket.indexOf(multiple): " + bucket.indexOf(multiple));
 
 			if(bucket.indexOf(multiple) >= 1){
 				let index = bucket.indexOf(multiple);
-				bucket.splice(index, 1);
+				seiveBucket.splice(index, 1);
+				console.log("bucket now contains: " + bucket);
 			}
 		//The sieve checks multiples of numbers in our array, use the loop to create an index, then use .indexOf() to see
 		// if this number appears i our array, if it does, .splice() it out
 
 		}
 	}
-
+	console.log("Final var bucket contains: " + bucket);
 	return "Loop as run it's course";
 }
 
-sumPrimes(5);
+sumPrimes(8);
