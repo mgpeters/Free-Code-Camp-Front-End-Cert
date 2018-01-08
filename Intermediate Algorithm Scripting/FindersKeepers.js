6e815bd9796523a54915de7eb43a1fd1b9df6b10
Matt Peters <matthew.george.peters@gmail.com>
2018-01-08 18:10:38 -0500

    completed challange

--- Intermediate Algorithm Scripting/FindersKeepers.js
+++ Intermediate Algorithm Scripting/FindersKeepers.js
@@ -7,17 +7,14 @@ element in the array that passes a truth test (second argument).
 
 */
 
+
 function findElement(arr, func) {
   var num = 0;
 
-  for(let n = 0; n < arr.length; n += 1){
-  	if(func(arr[n])){
-  		num = arr[n];
-  		break;
-  	}
-  }
-
-  return num;
+  num = arr.filter(func);
+  console.log(num);
+	
+	return num[0];
 }
 
 findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
