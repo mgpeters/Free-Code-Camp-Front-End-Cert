155c1277ac91e34f6ee3492ed0cbf9daae4d4c79
Matt Peters <matthew.george.peters@gmail.com>
2018-04-23 16:31:49 -0400

    modified Dropit - Started steamroller

--- Intermediate Algorithm Scripting/DropIt.js
+++ Intermediate Algorithm Scripting/DropIt.js
@@ -1,19 +1,10 @@
-/* 
-
-Drop It
-
-Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
+/*Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
 
 The second argument, func, is a function you'll use to test the first elements of the array to decide if you should drop it or not.
 
 Return the rest of the array, otherwise return an empty array.
 
-dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
-dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1].
-dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3].
-dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return [].
-dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4].
-dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2].
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
 
 Here are some helpful links:
 
@@ -21,12 +12,35 @@ Arguments object
 Array.prototype.shift()
 Array.prototype.slice()
 
+
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1].
+dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3].
+dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return [].
+dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4].
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2].
 */
+
 
 
 function dropElements(arr, func) {
   // Drop them elements.
-  return arr;
+	var rounds = arr.length;
+
+	for(let x = 0; x < rounds; x += 1){
+		if(!func(arr[0])){
+			arr.shift();
+        	}
+		else{
+			break;
+		}
+    }
+    return arr;
 }
 
-dropElements([1, 2, 3], function(n) {return n < 3; });
+console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}));
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}));
+console.log(dropElements([1, 2, 3], function(n) {return n > 0;}));
+console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));
+console.log(dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}));
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}));
